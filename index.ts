b746b07668bdd9e3756664b3cb754272d32a8b54
Abhishek-Sharma-069 <abhello12@gmail.com>
2026-02-22 21:00:25 +0530

    added vercel configuration

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ app.use(cors());
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST"]
   }
 });
